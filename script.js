627c2cd258270623ec177faf84c2ac43c770476b
ayoub-bensassi <bensassiayoub02@gmail.com>
2025-11-30 19:44:46 +0100

    update

--- script.js
+++ script.js
@@ -19,7 +19,16 @@ navLinks.addEventListener('click', (e) => {
 
 
 const products = [
-  { name: "Minimal Aesthetic Wallpapers Pack (10 HD Images)", price: "$1", image: "images/cover-square.jpg", link : "https://bensassiayoub2.gumroad.com/l/iyshsv" },
+  { name: "Tanjiro Kamado Akaza Wallpaper", price: "$1", image: "images/wall1.jpg", link : "https://starpixel0.gumroad.com/l/TanjiroKamado" },
+  { name: "Black Asta Black Clover Wallpaper", price: "$1", image: "images/wall2.jpg", link : "https://starpixel0.gumroad.com/l/BlackAsta" },
+  { name: "Skull American flag Wallpaper", price: "$1", image: "images/wall3.png", link : "https://starpixel0.gumroad.com/l/SkullAmericanflag" },
+  { name: "Naruto Uzumaki Smiley face Wallpaper", price: "$1", image: "images/wall4.jpg", link : "https://starpixel0.gumroad.com/l/NarutoUzumaki" },
+  { name: "Toyota GR Supra City street Wallpaper", price: "$1", image: "images/wall5.jpg", link : "https://starpixel0.gumroad.com/l/ToyotaGRSupra" },
+  { name: "Mitsuri Kanroji Neon art Wallpaper", price: "$1", image: "images/wall6.png", link : "https://starpixel0.gumroad.com/l/MitsuriKanroji" },
+  { name: "Lilo & Stitch 3D Wallpaper", price: "$1", image: "images/wall7.jpg", link : "https://starpixel0.gumroad.com/l/LiloStitch" },
+  { name: "Goku Ultra Instinct Electric Wallpaper", price: "$1", image: "images/wall8.png", link : "https://starpixel0.gumroad.com/l/GokuUltraInstinct" },
+  { name: "Sung Jinwoo Anime poster Wallpaper", price: "$1", image: "images/wall9.jpg", link : "https://starpixel0.gumroad.com/l/SungJinwoo" },
+  { name: "Urban Automotive Mercedes-AMG G 63 2025 Wallpaper", price: "$1", image: "images/wall10.jpg", link : "https://starpixel0.gumroad.com/l/UrbanAutomotive" }
 ];
 
 const container = document.querySelector(".product-list");
